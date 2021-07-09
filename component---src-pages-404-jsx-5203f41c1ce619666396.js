23721e697481a625b033639d8f0e15e66dd39718
kylehoehns <kylehoehns@users.noreply.github.com>
2021-07-09 20:12:20 +0000

    deployed via Gatsby Publish Action 🎩 for 4949bfb846eea6844fc9da54cdab9e6de9cc1a68

---
+++ component---src-pages-404-jsx-5203f41c1ce619666396.js
@@ -0,0 +1,2 @@
+(self.webpackChunkdevfolio=self.webpackChunkdevfolio||[]).push([[256],{1657:function(e,t,n){"use strict";n.r(t);var l=n(7294),o=n(5444),u=n(8208),a=n(1214),r="text-lg font-bold",s="underline";t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"Not found"}),l.createElement("h1",{className:r},"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist."," ",l.createElement(o.Link,{className:s,to:"/"},"Return to safety"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-5203f41c1ce619666396.js.map
